dc97aa8d9d7a6f64b69539dcc49ee95d68bfa814
Mohammad <mricky.it@gmail.com>
2019-12-03 15:01:39 +0700

    complete simple calculator

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nodejs-calculator
 BMI calculator
+------------------
+````
+npm install express --save
+
+npm install body-parser
+
+untuk mengambil parameter dari form data, menghasilkan object json
+````
--- calculator.js
+++ calculator.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({exptended:true}));
 app.get("/", function(req, res){
 	//res.send("<h1>Hello Calculator</h1>");
 	res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res){
+
+	var num1 = Number(req.body.num1);
+	var num2 = Number(req.body.num2);
+
+	var result = num1 + num2;
+
+	//res.send(req.body.num1);
+	res.send("Thre result of calculaton is " + result);	
+})
 
 
 app.listen(3000, ()=> console.log("Server running on port 3000"));	
