8e9bc7ebb2fd7719638adbe250474dd96920d1e9
Mohammad <mricky.it@gmail.com>
2019-12-03 15:12:53 +0700

    Complete Chalenge Routing BMI Calculator

--- calculator.js
+++ calculator.js
@@ -20,5 +20,19 @@ app.post("/", function(req, res){
 	res.send("Thre result of calculaton is " + result);	
 })
 
+app.get("/bmicalculator", function(req, res){
+	//res.send("<h1>Hello Calculator</h1>");
+	res.sendFile(__dirname + "/bmicalculator.html");
+})
+app.post("/bmicalculator", function(req, res){
+	var num1 = Number(req.body.num1);
+	var num2 = Number(req.body.num2);
+
+	var result = num1 + num2;
+
+	res.send("The result of bmi calculation is " + result);
+
+});
+
 
 app.listen(3000, ()=> console.log("Server running on port 3000"));	
